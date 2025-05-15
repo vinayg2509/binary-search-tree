7dbefa04cc36cf57d6eb8919401c6e2e9c2a4889
vinayg2509 <vinayg2509@gmail.com>
2025-05-15 13:49:52 +0530

    [Vinay] Added functionality to create BST with root 56 and children 70 and 30 and to print the same tree

--- src/main/java/BinarySearchTree.java
+++ src/main/java/BinarySearchTree.java
@@ -1,4 +1,55 @@
-public class BinarySearchTree
-{
+public class BinarySearchTree<E extends Comparable> {
+    static class Node<E extends Comparable> {
+        E data;
+        Node leftNode, rightNode;
 
+        Node(E data) {
+            this.data = data;
+        }
+    }
+
+    public Node insert(Node<E> root, E value) {
+        if (root == null) {
+            return new Node(value);
+        }
+        if (root.data.compareTo(value) > 0) {
+            root.leftNode = insert(root.leftNode, value);
+        }
+        if (root.data.compareTo(value) < 0) {
+            root.rightNode = insert(root.rightNode, value);
+        }
+        return root;
+
+    }
+
+    public void  inOrder(Node<E> root)
+    {
+        if (root == null) {
+            return ;
+        }
+
+        inOrder(root.leftNode);
+        System.out.print(root.data+" ");
+        inOrder(root.rightNode);
+    }
+
+
+
+    public static void main(String[] args) {
+
+        Node root = null;
+        int[] val = {56, 30, 70};
+        BinarySearchTree binarySearchTree= new BinarySearchTree();
+        for (int i = 0; i < val.length; i++) {
+            root = binarySearchTree.insert(root, val[i]);
+        }
+
+        binarySearchTree.inOrder(root);
+    }
 }
+
+
+
+
+
+
